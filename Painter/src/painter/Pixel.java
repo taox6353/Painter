c4dfb131ed48717065681d94b2e0e46c6a224953
taox6353 <taox6353@CA-SU-F106-28.sduhsd.lan>
2018-04-27 15:00:39 -0700

    Worked on Pixel

--- Painter/src/painter/Pixel.java
+++ Painter/src/painter/Pixel.java
@@ -7,13 +7,18 @@ import java.io.File;
 import javax.imageio.ImageIO;
 
 public class Pixel {
+	int x = 0;
+	int y = 0;
+	Color col;
 	public Pixel(){
-		
+		col = Color.WHITE;
 	}
-	public Pixel(int x,int y){
-		
+	public Pixel(int xCoord,int yCoord){
+		x = xCoord;
+		y = yCoord;
+		col = Color.WHITE;
 	}
-	public void draw(Graphics window){
-		
+	public void draw(Graphics window,Color color){
+		col = color;
 	}
 }
